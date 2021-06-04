cdb71293719890e4d08ed4a48e7bedf5f7c2f5ca
Tyler Rau <the.real.tyler.rau@gmail.com>
2021-06-04 03:31:04 -0500

    Revert "Apply preview-bg color to preview border of all shapes"
    This reverts commit c66e382210d2e9699b64a4e449ad646659a2e12b.

--- src/tui/light.go
+++ src/tui/light.go
@@ -693,17 +693,13 @@ func (w *LightWindow) drawBorder() {
 }
 
 func (w *LightWindow) drawBorderHorizontal(top, bottom bool) {
-	color := ColBorder
-	if w.preview {
-		color = ColPreviewBorder
-	}
 	if top {
 		w.Move(0, 0)
-		w.CPrint(color, repeat(w.border.horizontal, w.width))
+		w.CPrint(ColBorder, repeat(w.border.horizontal, w.width))
 	}
 	if bottom {
 		w.Move(w.height-1, 0)
-		w.CPrint(color, repeat(w.border.horizontal, w.width))
+		w.CPrint(ColBorder, repeat(w.border.horizontal, w.width))
 	}
 }
 
@@ -712,18 +708,14 @@ func (w *LightWindow) drawBorderVertical(left, right bool) {
 	if !left || !right {
 		width++
 	}
-	color := ColBorder
-	if w.preview {
-		color = ColPreviewBorder
-	}
 	for y := 0; y < w.height; y++ {
 		w.Move(y, 0)
 		if left {
-			w.CPrint(color, string(w.border.vertical))
+			w.CPrint(ColBorder, string(w.border.vertical))
 		}
-		w.CPrint(color, repeat(' ', width))
+		w.CPrint(ColBorder, repeat(' ', width))
 		if right {
-			w.CPrint(color, string(w.border.vertical))
+			w.CPrint(ColBorder, string(w.border.vertical))
 		}
 	}
 }
